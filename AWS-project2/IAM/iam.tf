b2569bbc596ba7683fc7a00cfe320a96a62a8035
ktalant <uchkunfan@gmail.com>
2020-01-04 22:03:17 -0500

    multiple users have been created and added to group

--- AWS-project2/IAM/iam.tf
+++ AWS-project2/IAM/iam.tf
@@ -7,3 +7,27 @@ tags = {
     Env = "dev"
   }
 }
+
+resource "aws_iam_user" "junai" {
+  name                  = "junai"
+  path                  = "/"
+  tags = {
+      Name = "junai"
+      env = "qa"
+  }
+}
+
+resource "aws_iam_group" "developers" {
+    name                = "developers"
+    path                = "/"
+}
+
+resource "aws_iam_group_membership" "dev_group" {
+  name                  = "dev-group-members"
+  users = [
+      aws_iam_user.talant.name
+      aws_iam.user.junai.name
+  ]
+}
+
+
