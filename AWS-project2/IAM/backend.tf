457adbbf57f904e14b3a353d0ca776069ce7b47f
ktalant <uchkunfan@gmail.com>
2020-01-04 23:44:02 -0500

    backend for iam has been added

--- AWS-project2/IAM/backend.tf
+++ AWS-project2/IAM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket              = "talant-terraform-backend"
-        key                 = "aws/iam/infrastructure"
+        key                 = "aws/iam/iam_state"
         region              = "us-east-1"
     }
 }
