7fe46ce5caba2cdbdd33784790fdc55917a4b083
ktalant <uchkunfan@gmail.com>
2020-01-05 20:38:54 -0500

    ec2-admin policy has been added

--- AWS-project2/IAM/iam.tf
+++ AWS-project2/IAM/iam.tf
@@ -54,3 +54,52 @@ resource "aws_iam_group_membership" "dev_group" {
   ]
   group = aws_iam_group.developers.name
 }
+resource "aws_iam_policy" "policy" {
+  name        = "test-policy"
+  description = "A test policy"
+
+  policy = <<EOF
+  {
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Action": "ec2:*",
+              "Effect": "Allow",
+              "Resource": "*"
+          },
+          {
+              "Effect": "Allow",
+              "Action": "elasticloadbalancing:*",
+              "Resource": "*"
+          },
+          {
+              "Effect": "Allow",
+              "Action": "cloudwatch:*",
+              "Resource": "*"
+          },
+          {
+              "Effect": "Allow",
+              "Action": "autoscaling:*",
+              "Resource": "*"
+          },
+          {
+              "Effect": "Allow",
+              "Action": "iam:CreateServiceLinkedRole",
+              "Resource": "*",
+              "Condition": {
+                  "StringEquals": {
+                      "iam:AWSServiceName": [
+                          "autoscaling.amazonaws.com",
+                          "ec2scheduled.amazonaws.com",
+                          "elasticloadbalancing.amazonaws.com",
+                          "spot.amazonaws.com",
+                          "spotfleet.amazonaws.com",
+                          "transitgateway.amazonaws.com"
+                      ]
+                  }
+              }
+          }
+      ]
+  }
+EOF
+}
