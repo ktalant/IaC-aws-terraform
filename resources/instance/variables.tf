4661734cb24a67e5a788f8d7e181b475f945d7ef
ktalant <uchkunfan@gmail.com>
2020-01-07 23:01:24 -0500

    vpc updated

--- resources/instance/variables.tf
+++ resources/instance/variables.tf
@@ -1,5 +1,4 @@
 variable "ec2_ami" {}
 variable "ec2_type" {}
-variable "app1_vpc_id" {}
 
 
--- resources/main.tf
+++ resources/main.tf
@@ -4,7 +4,6 @@ module "app1_instance" {
 
   ec2_ami           = "ami-00068cd7555f543d5"
   ec2_type          = "t2.medium"
-  app1_vpc_id       = "vpc-0c1fb90bbb0e2985c"
 }
 
 
