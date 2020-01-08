df5a4c14b1bbf6f62eb7781e8681589f14a12109
ktalant <uchkunfan@gmail.com>
2020-01-07 22:40:17 -0500

    instance module developed

--- resources/instance/ec2.tf
+++ resources/instance/ec2.tf
@@ -1,13 +1,14 @@
 resource "aws_key_pair" "dev_key" {
-    key_name      = var.ec2_keyname
+    key_name      = "devkey"
     public_key    = "~/.ssh/id_rsa.pub"
 
-    key_name      = aws_key_pair.dev_key.key_name
 }
 
 resource "aws_instance" "app1_ec2" {
   ami           = var.ec2_ami
   instance_type = var.ec2_type
+  key_name      = aws_key_pair.dev_key.key_name
+
 
   tags = {
     Name = "app1"
