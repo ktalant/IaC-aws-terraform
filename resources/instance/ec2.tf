546fd438c51caa479f1017794b47f5f41831b273
ktalant <uchkunfan@gmail.com>
2020-01-07 22:54:07 -0500

    key name on ec2 updated

--- resources/instance/ec2.tf
+++ resources/instance/ec2.tf
@@ -7,7 +7,7 @@ resource "aws_key_pair" "new_dev_key" {
 resource "aws_instance" "app1_ec2" {
   ami           = var.ec2_ami
   instance_type = var.ec2_type
-  key_name      = aws_key_pair.dev_key.key_name
+  key_name      = aws_key_pair.new_dev_key.key_name
 
 
   tags = {
