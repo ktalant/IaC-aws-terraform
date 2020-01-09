bbedeea90a89f4cde25a4d19522935f788dbc6c6
ktalant <uchkunfan@gmail.com>
2020-01-09 10:26:27 -0500

    public subnet has been inititated

--- VPC/outputs.tf
+++ VPC/outputs.tf
@@ -1,3 +1,6 @@
 output "vpcid" {
-  value = data.aws_vpc.selected.id
+  value = data.aws_vpc.my_vpc.id
+}
+output "public1cdir" {
+  value = aws_subnet.public_sub1.cidr_block
 }
--- VPC/vpc.tf
+++ VPC/vpc.tf
@@ -1,5 +1,10 @@
-data "aws_vpc" "selected" {
-  id = "${var.vpc_id}"
+data "aws_vpc" "my_vpc" {
+  id = var.vpc_id
 }
 
+resource "aws_subnet" "public_sub1" {
+  vpc_id            = data.aws_vpc.selected.id
+  availability_zone = var.az1
+  cidr_block        = cidrsubnet(data.aws_vpc.my_vpc.cidr_block, 4, 1)
+}
 
