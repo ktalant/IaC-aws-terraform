1204c7be83404548cbc81cbbb026795bea73bc6c
ktalant <uchkunfan@gmail.com>
2020-01-09 10:37:42 -0500

    vpc has been created

--- VPC/outputs.tf
+++ VPC/outputs.tf
@@ -4,3 +4,6 @@ output "vpcid" {
 output "public1cdir" {
   value = aws_subnet.public_sub1.cidr_block
 }
+output "my_vpc-cidr_block" {
+  value = "${data.aws_vpc.my_vpc.cidr_block}"
+}
--- VPC/vpc.tf
+++ VPC/vpc.tf
@@ -3,8 +3,10 @@ data "aws_vpc" "my_vpc" {
 }
 
 resource "aws_subnet" "public_sub1" {
-  vpc_id            = data.aws_vpc.my_vpc.id
-  availability_zone = var.az1
-  cidr_block        = cidrsubnet(data.aws_vpc.my_vpc.cidr_block, 4, 1)
+  vpc_id                    = data.aws_vpc.my_vpc.id
+  availability_zone         = var.az1
+  cidr_block                = cidrsubnet(data.aws_vpc.my_vpc.cidr_block, 4, 1)
+  map_public_ip_on_launch   = true
 }
+
 
