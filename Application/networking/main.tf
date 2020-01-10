03a8de3e3d557ddfdf1df872141f739ed6e2630e
ktalant <uchkunfan@gmail.com>
2020-01-09 20:01:13 -0500

    new vpc has been created

---
+++ Application/networking/main.tf
@@ -0,0 +1,41 @@
+data "aws_availability_zones" "availability" {}
+
+resource "aws_vpc" "wp_vpc" {
+  enable_dns_hostnames      = true
+  enable_dns_support        = true
+
+  tags  = {
+      Name                  = "wp-vpc"
+  }
+}
+
+
+resource "aws_security_group" "ssh-sg" {
+  name        = "allow_ssh"
+  description = "Allow port 22"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  # SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.accessip
+  }
+
+  # HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = var.accessip
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
