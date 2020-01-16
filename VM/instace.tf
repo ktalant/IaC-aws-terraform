9912c995276ec04b0d3c7918a645f1663d83afd0
ktalant <uchkunfan@gmail.com>
2020-01-16 10:48:09 -0500

    updated

--- VM/instace.tf
+++ VM/instace.tf
@@ -1,32 +1,5 @@
-resource "aws_iam_user" "ulan" {
-  name = "ulan"
-  path = "/"
-
-  tags = {
-    Name = "ulan-user"
-  }
-}
-
-resource "aws_iam_access_key" "ulan_accesskey" {
-  user = aws_iam_user.ulan.name
-}
-
-resource "aws_iam_user_policy" "ulan_ro" {
-  name = "ulan-ec-2ro"
-  user = aws_iam_user.ulan.name
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "ec2:Describe*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
+resource "aws_instance" "instance" {
+  ami               = var.ami
+  instance_type     = var.instance_type
+  key_name          = var.key_name
 }
