315c59778bfac5c9dc4dcdc2ac08b6d88610e6b2
ktalant <uchkunfan@gmail.com>
2020-01-23 12:45:52 -0500

    bucket has been created

---
+++ storage/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# Generate random numbers for buckets
+provider "random_id" "bucket_id" {
+  byte_length       = 4
+}
+
+# Create the bucket
+resource "aws_s3_bucket" "my_storage" {
+    bucket        = "${var.bucket_name}-${random_id.bucket_id.dec}"
+    acl           = "private"
+
+    force_destroy =  true
+
+    tags {
+      Name = var.bucket_name
+    }
+}
