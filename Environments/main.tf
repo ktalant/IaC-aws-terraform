3a5727eeb2e37dc7a9d3013acab70d6bb36bef35
ktalant <uchkunfan@gmail.com>
2020-01-27 21:58:08 -0500

    updated

--- Environments/main.tf
+++ Environments/main.tf
@@ -2,20 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "storage-east1" {
+module "storage1" {
   source       = "./storage"
   bucket_name = var.bucket_name
-  aws_region   = var.useast1
-}
-
-module "storage-east2" {
-  source       = "./storage"
-  bucket_name = var.bucket_name
-  aws_region   = var.useast2
-}
-
-module "storage-west1" {
-  source       = "./storage"
-  bucket_name = var.bucket_name
-  aws_region   = var.uswest1
+  aws_region   = var.aws_region
 }
--- Environments/outputs.tf
+++ Environments/outputs.tf
@@ -1,11 +1,3 @@
 output "bucketnames-useast1" {
   value = module.storage-east1.bucketname
 }
-
-output "bucketnames-useast2" {
-  value = module.storage-east2.bucketname
-}
-
-output "bucketnames-west1" {
-  value = module.storage-west1.bucketname
-}
