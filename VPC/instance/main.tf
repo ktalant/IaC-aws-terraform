b817500aba549d651b4c0ce371e211a0c4de2550
ktalant <uchkunfan@gmail.com>
2020-02-15 08:15:03 -0500

    updated

--- VPC/instance/main.tf
+++ VPC/instance/main.tf
@@ -7,7 +7,7 @@ resource "aws_key_pair" "talant_keypair" {
     public_key = file(var.key_path)
 }
 
-data "aws_ami" "example" {
+data "aws_ami" "talant_ami" {
   most_recent = true
   owners           = ["amazon"]
 
@@ -26,14 +26,14 @@ data "aws_ami" "example" {
     values = ["hvm"]
   }
 }
-# resource "aws_instance" "talant_vm" {
-#     instance_type = var.instance_type
-#     ami = var.ami_id
-#
-#     tags = {
-#       Name = "talant-vm"
-#     }
-#     key_name = aws_key_pair.talant_keypair.id
-#     vpc_security_group_ids = [module.networking.aws_security_group.talant_sg_new.id]
-#     subnet_id = module.networking.aws_subnet.wp_public_subnet1.id
-#   }
+resource "aws_instance" "talant_vm" {
+    instance_type = var.instance_type
+    ami = data.aws_ami.talant_ami.id
+
+    tags = {
+      Name = "talant-vm"
+    }
+    key_name = aws_key_pair.talant_keypair.id
+    vpc_security_group_ids = [module.networking.aws_security_group.talant_sg_new.id]
+    subnet_id = module.networking.aws_subnet.talant_public_subnet.*.id[0]
+  }
